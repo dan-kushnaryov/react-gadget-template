3241bdf57134ac45f4ceccb689d0bb6b40c5c60a
Mike Bannister <notimpossiblemike@gmail.com>
2014-11-28 10:51:31 +0000

    Shortcut for renderComponent

--- test/components/hello_react_spec.jsx
+++ test/components/hello_react_spec.jsx
@@ -3,17 +3,18 @@ var HelloReact = require('../../js/components/hello_react');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var findComponent = TestUtils.findRenderedDOMComponentWithClass;
+var renderComponent = TestUtils.renderIntoDocument;
 
 describe('HelloReact', function() {
   it('should render', function() {
-    var component = TestUtils.renderIntoDocument(<HelloReact />);
+    var component = renderComponent(<HelloReact />);
     var foundComponent = findComponent(component, 'hello-react');
     foundComponent.should.exist();
   });
 
   describe.skip('editable', function() {
     it('should indicate the user is an author', function() {
-      var component = TestUtils.renderIntoDocument(<HelloReact />);
+      var component = renderComponent(<HelloReact />);
       var foundComponent = findComponent(component, 'role');
       foundComponent.props.children.should.eq('author');
     });
@@ -21,7 +22,7 @@ describe('HelloReact', function() {
 
   describe('not editable', function() {
     it('should indicate the user is an learner', function() {
-      var component = TestUtils.renderIntoDocument(<HelloReact />);
+      var component = renderComponent(<HelloReact />);
       var foundComponent = findComponent(component, 'role');
       foundComponent.props.children.should.eq('learner');
     });
