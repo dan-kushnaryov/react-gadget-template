52581c9303996137e6a1525672eb5f5156ed504d
Mike Bannister <notimpossiblemike@gmail.com>
2014-11-29 02:19:35 -0800

    Cleanup and default timeout for e2e tests

--- tests/demos/sanity.js
+++ tests/demos/sanity.js
@@ -1,30 +1,30 @@
 var TestPlayer = require('./lib/test_player');
+var player = new TestPlayer();
 
 module.exports = {
-  beforeEach: function(client, callback) {
-    this.player = new TestPlayer();
-    this.player.start();
-    callback();
+  beforeEach: function(client) {
+    player = new TestPlayer();
+    player.start();
   },
 
   afterEach: function(callback) {
-    this.player.stop(callback);
+    player.stop(callback);
   },
 
   'Default authoring': function(client) {
-   this.player.visit(client)
-      .waitForElementPresent('.tray-gadget', 2000)
+    player.visit(client)
+      .waitForElementPresent('.tray-gadget')
       .execute("window.$('.tray-gadget:nth-child(1)').trigger('dblclick')")
-      .waitForElementPresent('.gadgetContent', 2000)
+      .waitForElementPresent('.gadgetContent')
       .saveScreenshot('images/default-authoring.png')
       .end();
   },
 
   'Default learning': function(client) {
-    this.player.visit(client)
-      .waitForElementPresent('.tray-gadget', 2000)
+    player.visit(client)
+      .waitForElementPresent('.tray-gadget')
       .execute("window.$('.tray-gadget:nth-child(1)').trigger('dblclick')")
-      .waitForElementPresent('.js-edit', 2000)
+      .waitForElementPresent('.js-edit')
       .click('.js-edit')
       .saveScreenshot('images/default-learning.png')
       .end();
