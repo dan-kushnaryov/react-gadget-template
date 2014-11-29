2b347683c651ad238a62b5a60e1616f5b7d14803
Mike Bannister <notimpossiblemike@gmail.com>
2014-11-29 15:12:39 -0800

    wip

--- tests/demos/lib/test_player.js
+++ tests/demos/lib/test_player.js
@@ -10,15 +10,34 @@ TestPlayer.prototype.visit = function(client) {
     .frame(0)
 };
 
-TestPlayer.prototype.start = function() {
-  this.preview = spawn('versal', ['preview', '--port', this.port]);
-  this.preview.stdout.pipe(process.stdout);
-  this.preview.stderr.pipe(process.stderr);
+TestPlayer.prototype.start = function(callback) {
+  callback();
+  // this.preview = spawn('versal', ['preview', '--port', this.port]);
+  // this.preview.stdout.on('data', function(data) {
+  //   if (data.toString().indexOf('http://localhost:'+this.port) >= 0) {
+  //     var args = ('-jar BrowserStackTunnel.jar -force ' + process.env.BROWSERSTACK_KEY + ' 127.0.0.1,' + this.port + ',0').split(' ');
+  //     console.log(args);
+  //     this.tunnel = spawn('java', args);
+  //     this.tunnel.stdout.on('data', function(data) {
+  //       if (data.toString().indexOf('You can now access your local server') >= 0) {
+  //         setTimeout(function() {callback();}, 10000);
+  //       }
+  //     }.bind(this));
+  //     this.tunnel.stdout.pipe(process.stdout);
+  //     this.tunnel.stderr.pipe(process.stderr);
+  //   }
+  // }.bind(this));
+  // this.preview.stdout.pipe(process.stdout);
+  // this.preview.stderr.pipe(process.stderr);
 };
 
 TestPlayer.prototype.stop = function(callback) {
-  this.preview.on('exit', callback);
-  this.preview.kill('SIGINT');
+  callback();
+  // this.preview.on('exit', function() {
+  //   this.tunnel.on('exit', callback);
+  //   this.tunnel.kill('SIGINT');
+  // }.bind(this));
+  // this.preview.kill('SIGINT');
 };
 
 module.exports = TestPlayer;
--- tests/demos/sanity.js
+++ tests/demos/sanity.js
@@ -4,35 +4,54 @@ var player = new TestPlayer();
 var ui = {
   gadgetInTray: '.tray-gadget',
   gadgetInLesson: '.gadgetContent',
-  gadgetEditToggle: '.js-edit'
+  gadgetEditToggle: '.js-edit',
+  gadgetDelete: '.js-trash',
+  gadgetConfirmDelete: '.js-delete'
 };
 
 module.exports = {
-  beforeEach: function(browser) {
+  before: function(browser, callback) {
     player = new TestPlayer();
-    player.start();
+    player.start(callback);
   },
 
-  afterEach: function(callback) {
+  after: function(browser, callback) {
     player.stop(callback);
   },
 
   'Default authoring': function(browser) {
     player.visit(browser)
+      // setup
       .waitForElementPresent(ui.gadgetInTray)
       .jqueryDoubleClick(ui.gadgetInTray)
       .waitForElementPresent(ui.gadgetInLesson)
+
+      // assert
       .saveScreenshot('images/default-authoring.png')
+
+
+      // cleanup
+      .click(ui.gadgetDelete)
+      .waitForElementPresent(ui.gadgetConfirmDelete)
+      .click(ui.gadgetConfirmDelete)
       .end();
   },
 
   'Default learning': function(browser) {
     player.visit(browser)
+      // setup
       .waitForElementPresent(ui.gadgetInTray)
       .jqueryDoubleClick(ui.gadgetInTray)
       .waitForElementPresent(ui.gadgetInLesson)
+
+      // assert
       .click(ui.gadgetEditToggle)
       .saveScreenshot('images/default-learning.png')
+
+      // cleanup
+      .click(ui.gadgetDelete)
+      .waitForElementPresent(ui.gadgetConfirmDelete)
+      .click(ui.gadgetConfirmDelete)
       .end();
   }
 };
