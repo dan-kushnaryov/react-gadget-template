50e076e583b4ae5a60c8d39dae6f8773991f9fbb
Mike Bannister <notimpossiblemike@gmail.com>
2014-11-29 02:33:17 -0800

    Ui dictionary

--- tests/demos/sanity.js
+++ tests/demos/sanity.js
@@ -1,6 +1,12 @@
 var TestPlayer = require('./lib/test_player');
 var player = new TestPlayer();
 
+var ui = {
+  gadgetInTray: '.tray-gadget',
+  gadgetInLesson: '.gadgetContent',
+  gadgetEditToggle: '.js-edit'
+};
+
 module.exports = {
   beforeEach: function(client) {
     player = new TestPlayer();
@@ -13,19 +19,19 @@ module.exports = {
 
   'Default authoring': function(client) {
     player.visit(client)
-      .waitForElementPresent('.tray-gadget')
-      .execute("window.$('.tray-gadget:nth-child(1)').trigger('dblclick')")
-      .waitForElementPresent('.gadgetContent')
+      .waitForElementPresent(ui.gadgetInTray)
+      .execute("window.$('" + ui.gadgetInTray + "').trigger('dblclick')")
+      .waitForElementPresent(ui.gadgetInLesson)
       .saveScreenshot('images/default-authoring.png')
       .end();
   },
 
   'Default learning': function(client) {
     player.visit(client)
-      .waitForElementPresent('.tray-gadget')
-      .execute("window.$('.tray-gadget:nth-child(1)').trigger('dblclick')")
-      .waitForElementPresent('.js-edit')
-      .click('.js-edit')
+      .waitForElementPresent(ui.gadgetInTray)
+      .execute("window.$('" + ui.gadgetInTray + "').trigger('dblclick')")
+      .waitForElementPresent(ui.gadgetInLesson)
+      .click(ui.gadgetEditToggle)
       .saveScreenshot('images/default-learning.png')
       .end();
   }
