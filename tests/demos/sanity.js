c5d110e46600b8ea4fc7fdbedcf30c760e2f19d1
Mike Bannister <notimpossiblemike@gmail.com>
2014-11-29 03:00:00 -0800

    Improve acceptance tests

--- tests/demos/sanity.js
+++ tests/demos/sanity.js
@@ -1,38 +1,54 @@
-var TestPlayer = require('./lib/test_player');
-var player = new TestPlayer();
-
 var ui = {
   gadgetInTray: '.tray-gadget',
   gadgetInLesson: '.gadgetContent',
-  gadgetEditToggle: '.js-edit'
+  gadgetEditToggle: '.js-edit',
+  gadgetDelete: '.js-trash',
+  gadgetConfirmDelete: '.js-delete'
 };
 
 module.exports = {
   beforeEach: function(browser) {
-    player = new TestPlayer();
-    player.start();
-  },
-
-  afterEach: function(callback) {
-    player.stop(callback);
+    browser
+      .url('http://127.0.0.1:3000')
+      .frame(0)
   },
 
   'Default authoring': function(browser) {
-    player.visit(browser)
+    browser
+      // setup
       .waitForElementPresent(ui.gadgetInTray)
       .jqueryDoubleClick(ui.gadgetInTray)
       .waitForElementPresent(ui.gadgetInLesson)
-      .saveScreenshot('images/default-authoring.png')
+
+      // assert
+      .pause(500).saveScreenshot('images/default-authoring.png')
+
+      // cleanup
+      .click(ui.gadgetDelete)
+      .waitForElementPresent(ui.gadgetConfirmDelete)
+      .pause(500)
+      .click(ui.gadgetConfirmDelete)
+      .waitForElementNotPresent(ui.gadgetInLesson)
       .end();
   },
 
   'Default learning': function(browser) {
-    player.visit(browser)
+    browser
+      // setup
       .waitForElementPresent(ui.gadgetInTray)
       .jqueryDoubleClick(ui.gadgetInTray)
       .waitForElementPresent(ui.gadgetInLesson)
+
+      // assert
       .click(ui.gadgetEditToggle)
-      .saveScreenshot('images/default-learning.png')
+      .pause(500).saveScreenshot('images/default-learning.png')
+
+      // cleanup
+      .click(ui.gadgetDelete)
+      .waitForElementPresent(ui.gadgetConfirmDelete)
+      .pause(500)
+      .click(ui.gadgetConfirmDelete)
+      .waitForElementNotPresent(ui.gadgetInLesson)
       .end();
   }
 };
