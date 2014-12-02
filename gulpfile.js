0951a8b7a3a530591658f3e07d844eb43b37ebb2
Mike Bannister <notimpossiblemike@gmail.com>
2014-12-01 21:54:02 -0800

    Improve build
    Cleaner stylus/css approach
    Use global react so we can benefit from debugging tools in addons

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
-var concatCSS = require('gulp-concat-css');
 var exec = require('child_process').exec;
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var Filter = require('gulp-filter');
+var globalShim = require('browserify-global-shim');
 
 gulp.task('jslint', function (cb) {
   exec('./scripts/lint.sh', function (err, stdout, stderr) {
@@ -21,27 +23,30 @@ gulp.task('browserify', function(){
     ignoreGlobals: true,
     extensions: ['.jsx']
   });
+  var globalShimTransform = globalShim.configure({
+    'react': 'React'
+  });
   b.transform(reactify);
+  b.transform(globalShimTransform);
   b.add('./js/app.jsx');
   return b.bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('stylus', function () {
-  gulp.src('./css/app.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./tmp/stylus'));
-});
+gulp.task('css', function() {
+  var filter = Filter('**/*.styl');
 
-gulp.task('concat-css', function() {
-  gulp.src([
-      'tmp/stylus/app.css',
+  return gulp.src([
       'node_modules/normalize.css/normalize.css',
-      'css/gadget.css'
+      'css/gadget.css',
+      './css/**/*.styl'
     ])
-    .pipe(concatCSS('app.css'))
-    .pipe(gulp.dest('dist/'))
+    .pipe(filter)
+    .pipe(stylus())
+    .pipe(filter.restore())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['jslint', 'browserify', 'stylus', 'concat-css']);
+gulp.task('default', ['jslint', 'browserify', 'css']);
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,8 +8,6 @@ JSX_BIN=./node_modules/react-tools/bin/jsx
 JSHINT_BIN=./node_modules/jshint/bin/jshint
 
 LINT_DIR=$(mktemp -d /tmp/jshint.XXXXXX)
-rm -rf $LINT_DIR
 cp -r js $LINT_DIR
 $JSX_BIN js/ $LINT_DIR/ --extension jsx
 $JSHINT_BIN $LINT_DIR
-rm -rf ./tmp
