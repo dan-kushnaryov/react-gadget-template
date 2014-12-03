d1b47ff4af0511ddc11eeda61370c2ed073c487c
Mike Bannister <notimpossiblemike@gmail.com>
2014-12-03 02:03:18 -0800

    Refactor demo script
    And delete images before each run

--- scripts/demos.js
+++ scripts/demos.js
@@ -7,8 +7,10 @@ var spawn = require('child_process').spawn;
 var https = require('https');
 var fs = require('fs-extra');
 var path = require('path');
+var async = require('async');
+var glob = require('glob');
 
-var startVersalPreview = function() {
+var startServerAndRunTests = function(callback) {
   var versalPreviewCommand = 'versal preview --port 6952'.split(' ');
 
   var command = versalPreviewCommand[0];
@@ -18,18 +20,24 @@ var startVersalPreview = function() {
 
   versalPreview.stdout.pipe(process.stdout);
   versalPreview.stderr.pipe(process.stderr);
-  versalPreview.stdout.on('data', onPreviewOutput.bind(this, versalPreview));
+
+  versalPreview.stdout.on('data',
+    onPreviewOutput.bind(null,
+      versalPreview,
+      callback
+    )
+  );
 };
 
-var onNightwatchOutput = function(versalPreview, nightwatch, data) {
+var onNightwatchOutput = function(versalPreview, nightwatch, callback, data) {
   if (/total assertions passed/.test(data.toString())) {
     versalPreview.kill('SIGINT');
     nightwatch.kill('SIGINT');
-    process.exit();
+    callback();
   }
 };
 
-var onPreviewOutput = function(versalPreview, data) {
+var onPreviewOutput = function(versalPreview, callback, data) {
   if (/ctrl \+ C to stop/.test(data.toString())) {
     var nightwatchArgs = process.argv.slice(2);
 
@@ -38,7 +46,12 @@ var onPreviewOutput = function(versalPreview, data) {
     nightwatch.stdout.pipe(process.stdout);
     nightwatch.stderr.pipe(process.stderr);
 
-    var handler = onNightwatchOutput.bind(null, versalPreview, nightwatch);
+    var handler = onNightwatchOutput.bind(null,
+      versalPreview,
+      nightwatch,
+      callback
+    );
+
     nightwatch.stdout.on('data', handler);
   }
 };
@@ -74,4 +87,20 @@ var ensureSeleniumJar = function(callback) {
   wgetSelenium.stdout.on('close', callback);
 };
 
-ensureSeleniumJar(startVersalPreview);
+var deleteImages = function(callback) {
+  var images = glob.sync('images/**.png');
+  images.forEach(fs.removeSync);
+  callback();
+};
+
+async.series([
+  deleteImages,
+  ensureSeleniumJar,
+  startServerAndRunTests
+], function(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.info('Done.');
+});
