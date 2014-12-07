2aebad5f59b0945ba8ae2b4bae03e618b7c7833d
Mike Bannister <notimpossiblemike@gmail.com>
2014-12-06 22:42:03 -0800

    Switch to gulp-shell

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,13 @@
-var browserify = require('browserify');
 var gulp = require('gulp');
+var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
-var spawn = require('child_process').spawn;
 var stylus = require('gulp-stylus');
 var concat = require('gulp-concat');
 var globalShim = require('browserify-global-shim');
+var shell = require('gulp-shell');
 
-gulp.task('lint', function (callback) {
-  var lint = spawn('./scripts/lint.sh');
-  lint.stdout.pipe(process.stdout);
-  lint.stderr.pipe(process.stderr);
-
-  lint.on('exit', function(code) {
-    callback();
-  });
-});
-
-gulp.task('run-demos', function (callback) {
-  // If this breaks down for you you can always run
-  // `versal preview --port 6952` and then `nightwatch`
-  // in a second terminal and forgo the automation.
-  var nightwatchArgs = process.argv.slice(2);
-  var demos = spawn('./scripts/demos.js', nightwatchArgs);
-  demos.stdout.pipe(process.stdout);
-  demos.stderr.pipe(process.stderr);
-
-  demos.on('exit', function(code) {
-    callback();
-  });
-});
-
-gulp.task('run-tests', function (callback) {
-  var tests = spawn('./node_modules/.bin/jest');
-  tests.stdout.pipe(process.stdout);
-  tests.stderr.pipe(process.stderr);
-
-  tests.on('exit', function(code) {
-    callback();
-  });
-});
+var args = process.argv.slice(2);
 
 gulp.task('bundle', function(){
   var b = browserify({
@@ -68,6 +36,10 @@ gulp.task('css', function() {
     .pipe(concat('app.css'))
     .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('lint', shell.task(['./scripts/lint.sh ' + args.join(' ')]));
+gulp.task('run-demos', shell.task(['./scripts/demos.js ' + args.join(' ')]));
+gulp.task('run-tests', shell.task(['./node_modules/.bin/jest ' + args.join(' ')]));
 
 gulp.task('base', ['lint', 'bundle', 'css']);
 gulp.task('demo', ['base', 'run-demos']);
