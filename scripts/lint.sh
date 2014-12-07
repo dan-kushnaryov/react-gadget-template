f4014c553c883f6c93ffe1060d4d8f0b4fa0f34f
Mike Bannister <notimpossiblemike@gmail.com>
2014-12-06 22:29:38 -0800

    Fix linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,9 @@ set -e
 JSX_BIN=./node_modules/react-tools/bin/jsx
 JSHINT_BIN=./node_modules/jshint/bin/jshint
 
-LINT_DIR=$(mktemp -d /tmp/jshint.XXXXXX)
+LINT_DIR=./tmp/jshint_src
+rm -rf $LINT_DIR
+mkdir -p $LINT_DIR
 cp -r js $LINT_DIR
-$JSX_BIN js/ $LINT_DIR/ --extension jsx
+$JSX_BIN js/ $LINT_DIR --extension jsx
 $JSHINT_BIN $LINT_DIR
